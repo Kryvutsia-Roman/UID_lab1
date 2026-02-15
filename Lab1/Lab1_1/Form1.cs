6fdfd8ff60a5983d8c0d647ff6ca73665b0633bb
Kryvutsia-Roman <120135274+Kryvutsia-Roman@users.noreply.github.com>
2026-02-15 19:19:43 +0200

    Add files via upload

---
+++ Lab1/Lab1_1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string answer = textBox1.Text;
+
+            if (answer == "")
+            {
+                MessageBox.Show("Введіть відповідь!");
+                return;
+            }
+
+            string path = "answers.txt";
+
+            File.AppendAllText(path, answer + Environment.NewLine);
+
+            MessageBox.Show("Відповідь збережено!");
+
+            textBox1.Clear();
+        }
+    }
+}
